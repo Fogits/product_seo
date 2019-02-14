a5d55b08752f350d03ea346f0bcabdb67b537b52
imed.chaabouni <imed.chaabouni@fogits.com>
2019-02-14 10:08:05 +0100

    set image for big screenshot

--- product_seo/__manifest__.py
+++ product_seo/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'Uncategorized',
     'version': '12.0',
-
+    'images': ['static/description/banner.png'],
     # any module necessary for this one to work correctly
     'depends': ['base','product','website_sale'],
 
